5fe0cc760dcbda26d4e5b7bcb7fc1350d30f5d6c
marinacor1 <marinaspam1234@gmail.com>
2016-04-07 11:48:15 -0600

    adding custom validation

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,2 +1,20 @@
 class Fan < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :name,  presence:   true,
+                    format:     { with: /\A[a-zA-Z]+\z/, message: "only allows uppercase and lowercase letters"}
+                    #adds message to let user know why it didn't work
+                    #errors don't happen unless you save it
+  validates :email, presence:   true,
+                  uniqueness: true,
+                  length:     { in: 5..50 }
+  validate :no_richards
+
+  def no_richards
+    if name.to_s.downcase == "richard"
+      errors.add(:name, "cannot be Richard") if name == "Richard"
+    end
+  end
+                    # validates_presence_of :name, :email
+  # validates: name, presence: true
 end
--- test/models/fan_test.rb
+++ test/models/fan_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
 
 class FanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def valid_attributes
+      {
+        name:               "Jorge",
+        email:              "yosoybelieber@example.com",
+      }
+    end
+
+  test "it creates a fan" do
+    result = Fan.new(valid_attributes) #always do new instead of create to save memory
+
+    assert result.valid?
+    assert_equal "Jorge", result.name
+    assert_equal "yosoybelieber@example.com", result.email
+  end
+
+  test "it cannot create a fan without an name" do
+    result = Fan.new(email: "yosoybelieber@example.com")
+
+    assert result.invalid?, "fan should be invalid"
+  end
+
+  test "it cannot create a fan with the same email" do
+    2.times { Fan.create(valid_attributes) }
+
+    result = Fan.where(email: "yosoybelieber@example.com")
+    assert_equal 1, result.count
+  end
+
+  test "it only accepts letters as a name" do
+    fan = Fan.new(
+      name:   "Jorge1",
+      email:  "yosoybelieber@example.com"
+      )
+
+    refute fan.valid?
+  end
+
+  test "it only accepts an email between 5 and 50 character" do
+    fan = Fan.new(
+      name: "Jorge",
+      email: "Jorj"
+    )
+  assert fan.invalid?
+end
+
+  test "it cannot create a fan named Richard" do
+    fan = Fan.new(
+      name:               "Richard",
+      email:              "richard@example.com"
+      )
+
+    refute fan.valid?
+    assert_includes fan.errors.full_messages, "Name cannot be Richard"
+
+  end
 end
