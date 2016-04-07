9337acf3d69e2e9d875ffb8426266ce8ff032659
marinacor1 <marinaspam1234@gmail.com>
2016-04-07 17:40:03 -0600

    status works

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,5 @@
 class Donation < ActiveRecord::Base
   belongs_to :fan
   validates :amount, numericality: {only_integer: true}
+  validates :status, inclusion: { in: %w(pending processed cancelled)}
 end
--- test/models/donation_test.rb
+++ test/models/donation_test.rb
@@ -3,16 +3,37 @@ require 'test_helper'
 class DonationTest < ActiveSupport::TestCase
   valid_donation_attributes = {
     amount: 10,
-    status: "paid"
+    status: "pending"
   }
   test "it creates donations only if they are whole numbers" do
     result = Donation.new(valid_donation_attributes)
     result2 = Donation.new( {
       amount: 9.99,
-      status: "paid"
+      status: "pending"
       })
 
     assert result.valid?
     refute result2.valid?
+  end
+
+  test "it creates donations that are processed pending or cancelled only" do
+    result = Donation.new(valid_donation_attributes)
+    result2 = Donation.new( {
+      amount: 11,
+      status: "happy"
+      })
+    result3 = Donation.new( {
+      amount: 11,
+      status: "cancelled"
+      })
+    result4 = Donation.new( {
+      amount: 11,
+      status: "processed"
+      })
+
+    assert result.valid?
+    assert result2.invalid?
+    assert result3.valid?
+    assert result4.valid?
   end
 end
